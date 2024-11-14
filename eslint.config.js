5b1ac34cb62c3684a02916ec16c41b111139b5ce
Tri Hargianto <trihargianto@gmail.com>
2024-11-14 22:13:59 +0700

    feat: setup eslint and commitizen
    reference: https://ota-meshi.github.io/eslint-plugin-astro/

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {},
+  },
+];
