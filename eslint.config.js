792aa080bfd4bf197409c3c33adc71b6b99fadae
Tri Hargianto <trihargianto@gmail.com>
2024-11-14 22:51:56 +0700

    feat: setup prettier
    reference: https://docs.astro.build/en/editor-setup/#prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import js from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
   {
     rules: {},
   },
