30ae6225da6df1e5a4b8c703e4ba75d12826351f
Tri Hargianto <trihargianto@gmail.com>
2024-11-16 11:42:21 +0700

    setup react and shadcnui framework (#21)
    * use globals.css for tailwind
    
    * run the shadcn init command
    
    ```
    pnpm dlx shadcn@latest init
    ```
    
    * install @astrojs/react
    
    ```
    pnpm astro add react
    ```

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,19 @@
 import catppuccin from "@catppuccin/tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
   plugins: [
     // https://github.com/catppuccin/tailwindcss/tree/main?tab=readme-ov-file#usage
@@ -12,5 +21,8 @@ export default {
       prefix: "ctp",
       defaultFlavour: "macchiato",
     }),
+
+    // https://github.com/jamiebuilds/tailwindcss-animate
+    tailwindcssAnimate,
   ],
 };
