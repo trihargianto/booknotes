bcc3bbfd4751d16c99b580f8bb96badc96b41ebe
Tri Hargianto <trihargianto@gmail.com>
2024-11-16 11:04:34 +0700

    setup catppuccin tailwindcss

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import catppuccin from "@catppuccin/tailwindcss";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // https://github.com/catppuccin/tailwindcss/tree/main?tab=readme-ov-file#usage
+    catppuccin({
+      prefix: "ctp",
+      defaultFlavour: "macchiato",
+    }),
+  ],
 };
