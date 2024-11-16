32c1c602410789acac69e0e5c9559b8c403313aa
Tri Hargianto <trihargianto@gmail.com>
2024-11-16 19:57:35 +0700

    create dark/light mode switcher

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-
 import { SunIcon, BookOpenIcon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
+  function toggleDarkMode() {
+    const darkClass = "ctp-machhiato";
+    const lightClass = "ctp-latte";
+
+    const element = document.body;
+
+    const isLight = element.classList.contains(lightClass);
+
+    if (isLight) {
+      document.body.classList.add(darkClass);
+      document.body.classList.remove(lightClass);
+    } else {
+      document.body.classList.add(lightClass);
+      document.body.classList.remove(darkClass);
+    }
+
+    localStorage.setItem("theme", isLight ? darkClass : lightClass);
+  }
+
   return (
     <nav className="flex justify-between items-center px-4 py-4 border-b border-b-ctp-surface0">
       <div className="inline-flex items-center">
@@ -11,8 +28,8 @@ export default function Navbar() {
         <span className="text-xl font-bold text-ctp-text">Book Notes</span>
       </div>
 
-      <button>
-        <SunIcon className="text-ctp-overlay1 size-10" />
+      <button onClick={toggleDarkMode}>
+        <SunIcon className="text-ctp-overlay1 size-9" />
       </button>
     </nav>
   );
