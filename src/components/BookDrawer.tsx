3a95810e21ae15363a013506d008c8ed8de66de8
Tri Hargianto <trihargianto@gmail.com>
2024-11-17 17:36:37 +0700

    put button component on book drawer

--- src/components/BookDrawer.tsx
+++ src/components/BookDrawer.tsx
@@ -2,6 +2,7 @@ import { Drawer, DrawerContent } from "@/components/ui/drawer";
 
 import type { BookType } from "@/types";
 import Tag from "@/components/Tag";
+import Button from "@/components/Button";
 
 interface Props {
   isOpen: boolean;
@@ -34,6 +35,44 @@ export default function BookDrawer({ isOpen, setOpen, book }: Props) {
         </div>
 
         <p className="text-ctp-text line-clamp-4">{book.synopsis}</p>
+
+        <div className="flex gap-2 mt-4">
+          <Button variant="secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
+            Buy
+          </Button>
+
+          <Button variant="primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+              />
+            </svg>
+            Read Note
+          </Button>
+        </div>
       </DrawerContent>
     </Drawer>
   );
